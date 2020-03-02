3aecd7a014970afb2d0161437b19d968cb26e5bf
eu-vinnom <vinnom.2d@gmail.com>
2020-03-02 11:26:06 -0300

    added split each line into array functionality

--- csv2json.js
+++ csv2json.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
 
 const csvFilePath = 'input.csv';
-const fileReader = fs.readFileSync(csvFilePath, 'utf-8');
 
-var lines = fileReader.split("\n");
+const fileReader = readLines(csvFilePath, fs);
+const lines = splitIntoLines(fileReader);
+const contentRaw = splitIntoContent(lines);
 
 console.log(fileReader);
 console.log(lines);
+console.log(contentRaw);
+
+function readLines(pathToFile, fileSystemInteractor) {
+    return fileSystemInteractor.readFileSync(pathToFile, 'utf-8');
+}
+
+function splitIntoLines(multiLinesString) {
+    return multiLinesString.split("\n");
+}
+
+function splitIntoContent(lineArray) {
+    let contentArray = [];
+    for (let index = 0; index < lineArray.length; index++) {
+        contentArray[index] = lineArray[index].split(",");
+    }
+    return contentArray;
+}
