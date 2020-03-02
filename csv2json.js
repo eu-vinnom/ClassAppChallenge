51d8a7024ae75a07497f2055de7dd9d20a37a3dd
eu-vinnom <vinnom.2d@gmail.com>
2020-03-02 13:33:55 -0300

    added functionality for getting fullnames from input

--- csv2json.js
+++ csv2json.js
@@ -1,27 +1,65 @@
 const fs = require('fs');
+const _ = require('lodash');
 
 const csvFilePath = 'input.csv';
 
 const fileReader = readLines(csvFilePath, fs);
-const lines = splitIntoLines(fileReader);
-const contentRaw = splitIntoContent(lines);
+const lineArray = oneArrayLines(fileReader);
+const nlinesArray = eachLineAsArray(lineArray);
+const contentArray = removeFalseyContent(nlinesArray);
+
+var fullname = new Set(getFullnamesStringArray(contentArray));
 
 console.log(fileReader);
-console.log(lines);
-console.log(contentRaw);
+console.log(lineArray);
+console.log(nlinesArray);
+console.log(contentArray);
+console.log(fullname);
 
 function readLines(pathToFile, fileSystemInteractor) {
     return fileSystemInteractor.readFileSync(pathToFile, 'utf-8');
 }
 
-function splitIntoLines(multiLinesString) {
+function oneArrayLines(multiLinesString) {
     return multiLinesString.split("\n");
 }
 
-function splitIntoContent(lineArray) {
-    let contentArray = [];
-    for (let index = 0; index < lineArray.length; index++) {
-        contentArray[index] = lineArray[index].split(",");
+function eachLineAsArray(oneLineArray) {
+    let linesArray = [];
+    for (let index = 0; index < oneLineArray.length; index++) {
+        linesArray[index] = oneLineArray[index].split(",");
     }
-    return contentArray;
+    return linesArray;
+}
+
+function removeFalseyContent(multilinesArray) {
+    let eachLineIsArray = [];
+    for (let index = 0; index < multilinesArray.length; index++) {
+        eachLineIsArray[index] = _.compact(multilinesArray[index]);
+    }
+    return eachLineIsArray;
+}
+
+function getFullnamesArrays(arrayOfContents) {
+    let fullnamesArray = [];
+    let nameAux = [];
+    for (let i = 0; i < arrayOfContents.length; i++) {
+        for (let j = 0; j < arrayOfContents[i].length; j++) {
+            nameAux[j] = arrayOfContents[i][j].split(" ");
+            nameAux[j] = _.compact(nameAux[j]);
+            if (nameAux[j].length === 3) {
+                fullnamesArray[i] = nameAux[j];
+            }
+        }
+    }
+    return _.compact(fullnamesArray);
+}
+
+function getFullnamesStringArray(arrayOfContents) {
+    let fullnameStringArray = [];
+    arrayOfFullnames = getFullnamesArrays(arrayOfContents);
+    for (let index = 0; index < arrayOfFullnames.length; index++) {
+        fullnameStringArray[index] = arrayOfFullnames[index].join(" ");
+    }
+    return fullnameStringArray;
 }
