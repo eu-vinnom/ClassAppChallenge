b1d64ab684b1f19b07792e7a9e1a459d46336dbc
eu-vinnom <vinnom.2d@gmail.com>
2020-03-04 14:26:10 -0300

    - now the program handle all categories, ie, object properties;  - the program will write a file called output.json with the gathered from input.csv.

--- csv2json.js
+++ csv2json.js
@@ -1,29 +1,27 @@
 const fs = require('fs');
 const _ = require('lodash');
+const PNF = require('google-libphonenumber').PhoneNumberFormat;
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
 
 const csvFilePath = 'input.csv';
+const jsonFilePath = 'teste.json';
 
 const fileReader = readLines(csvFilePath, fs);
 const lineArray = oneArrayLines(fileReader);
 const nlinesArray = eachLineAsArray(lineArray);
 const contentArray = removeEmptyFromFirstLine(nlinesArray);
 
-var fullnames = new Set(getFullnames(contentArray));
-var eids = new Set(getEids(contentArray));
-var classes = new Set(getClasses(contentArray));
-var invisible = getInvisible(contentArray);
-var seeAll = getSeeAll(contentArray);
+const fullname = new Set(getFullnames(contentArray));
+const eid = new Set(getEids(contentArray));
+const classes = new Set(getClasses(contentArray));
+const invisible = getInvisible(contentArray);
+const seeAll = getSeeAll(contentArray);
+const addresses = getAddresses(contentArray);
+const json = getJsonObject();
 
-// console.log(fileReader);
-// console.log(lineArray);
-// console.log(nlinesArray);
-// console.log(contentArray[3].length);
+const jsonString = JSON.stringify(json, null, 4);
 
-console.log(fullnames);
-console.log(eids);
-console.log(classes);
-console.log(invisible);
-console.log(seeAll);
+fs.writeFileSync(jsonFilePath, jsonString);
 
 function readLines(pathToFile, fileSystemInteractor) {
     return fileSystemInteractor.readFileSync(pathToFile, 'utf-8');
@@ -53,7 +51,7 @@ function eachLineAsArray(oneLineArray) {
     for (let line = 0; line < linesArray.length; line++) {
         linesArray[line] = _.compact(linesArray[line]);
     }
-    
+
     return linesArray;
 }
 
@@ -70,8 +68,21 @@ function removeEmptyFromFirstLine(multilinesArray) {
     }
     arrayOfContents = multilinesArray;
     arrayOfContents[line] = _.compact(firstLineArray);
+    removeQuotesFromContent(arrayOfContents);
 
     return arrayOfContents;
+}
+
+function removeQuotesFromContent(arrayOfContents) {
+    let quote = "\"";
+
+    for (let line = 0; line < arrayOfContents.length; line++) {
+        for (let column = 0; column < arrayOfContents[line].length; column++) {
+            if (arrayOfContents[line][column].startsWith(quote) && arrayOfContents[line][column].endsWith(quote)) {
+                arrayOfContents[line][column] = arrayOfContents[line][column].substring(1, arrayOfContents[line][column].length - 1);
+            }
+        }
+    }
 }
 
 function getCategoryColumns(arrayOfContents, categoryNameRegex) {
@@ -104,11 +115,11 @@ function setValues(arrayOfContents, categoryNameRegex) {
         }
         line++;
     }
-    
+
     return dataArray;
 }
 
-function mapCategoryByFullname(arrayOfContents, fullnameSet, categoryNameRegex) {
+function mapCategoryByFullname(arrayOfContents, categoryNameRegex) {
     let index = 0;
     let key = 0;
     let dataLine = 1;
@@ -122,21 +133,23 @@ function mapCategoryByFullname(arrayOfContents, fullnameSet, categoryNameRegex) 
         for (let column = 0; column < arrayOfContents[line].length; column++) {
             if (column === category[column] && dataArray[dataLine] == undefined) {
                 dataArray[dataLine] = arrayOfContents[line][column];
-            } else if (column === category[column] && dataArray[dataLine] != undefined) {
-                dataArray[dataLine] = dataArray[dataLine].concat(arrayOfContents[line][column])
+                continue;
+            }
+            if (column === category[column] && dataArray[dataLine] != undefined) {
+                dataArray[dataLine] = dataArray[dataLine].concat(arrayOfContents[line][column]);
             }
         }
-        line++;
-        index++;
         dataLine = dataLine + 2;
         key = key + 2;
+        line++;
+        index++;
     }
 
+    dataArray = _.compact(dataArray);
     return _.chunk(dataArray, 2);
-    
 }
 
-function setMappedValues(categoryArray) {
+function getMappedValues(categoryArray) {
     let mappedArray = [];
     let line = 0;
     let columnKey = 0;
@@ -147,7 +160,7 @@ function setMappedValues(categoryArray) {
             mappedArray[line] = categoryArray[line][columnValue];
         } if (categoryArray[line + 1] != undefined) {
             if (categoryArray[line][columnKey] === categoryArray[line + 1][columnKey]) {
-                mappedArray[line] = mappedArray[line].concat(" ",categoryArray[line + 1][columnValue]);
+                mappedArray[line] = mappedArray[line].concat(" ", categoryArray[line + 1][columnValue]);
                 line++;
             }
         }
@@ -156,6 +169,63 @@ function setMappedValues(categoryArray) {
     mappedArray = _.compact(mappedArray);
 
     return mappedArray;
+}
+
+function mapPhoneAddress(arrayOfContents) {
+    let dataLine = 0;
+    let dataColumn = 1;
+    let phoneNumberKey = 2;
+    let line = 1;
+    let mappedArray = [];
+
+    while (line < arrayOfContents.length) {
+        for (let column = 0; column < arrayOfContents[line].length; column++) {
+            if (phoneUtil.isPossibleNumberString(arrayOfContents[line][column], 'BR')) {
+                mappedArray[dataLine] = line;
+                mappedArray[dataColumn] = column;
+                mappedArray[phoneNumberKey] = arrayOfContents[line][column];
+            }
+            dataLine = dataLine + 3;
+            dataColumn = dataColumn + 3;
+            phoneNumberKey = phoneNumberKey + 3;
+        }
+        line++;
+    }
+
+    mappedArray = _.compact(mappedArray);
+    return _.chunk(mappedArray, 3);
+}
+
+function mapEmailAddress(arrayOfContents) {
+    let dataLine = 0;
+    let dataColumn = 1;
+    let emailKey = 2;
+    let line = 1;
+    let mappedArray = [];
+
+    while (line < arrayOfContents.length) {
+        for (let column = 0; column < arrayOfContents[line].length; column++) {
+            if (isAValidEmail(arrayOfContents[line][column])) {
+                mappedArray[dataLine] = line;
+                mappedArray[dataColumn] = column;
+                mappedArray[emailKey] = arrayOfContents[line][column];
+            }
+            dataLine = dataLine + 3;
+            dataColumn = dataColumn + 3;
+            emailKey = emailKey + 3;
+        }
+        line++;
+    }
+
+    mappedArray = _.compact(mappedArray);
+    return _.chunk(mappedArray, 3);
+}
+
+function isAValidEmail(emailAddress) {
+    dotCom = ".com";
+    dotComDotBr = ".com.br";
+
+    return (emailAddress.endsWith(dotCom)) || (emailAddress.endsWith(dotComDotBr));
 }
 
 function checkIfTrueofFalse(mappedArray) {
@@ -172,7 +242,7 @@ function checkIfTrueofFalse(mappedArray) {
             for (let column = 0; column < testValidArray[line].length; column++) {
                 if (testValidArray[line][column] == one || testValidArray[line][column] == yes) {
                     validArray[line] = true;
-                } else {
+                } else if (validArray[line] == undefined) {
                     validArray[line] = false;
                 }
             }
@@ -196,6 +266,172 @@ function getFormattedArray(mappedArray, regex) {
     return formattedArray;
 }
 
+function mergeAddressCategory(phoneAddressMap, emailAddressMap, addressType, arrayOfContents) {
+    let line = 0;
+    let typeIndex = 1;
+    let tagIndex = 2;
+    let addressIndex = 3;
+    let keyLine = 0;
+    let keyColumn = 0;
+    let tagColumn = 1;
+    let addressColumn = 2;
+    let mappedAddressesArray = [];
+    let phone = 0;
+    let email = 1;
+
+    let phoneArrayMapped = [];
+    let emailArrayMapped = [];
+
+    while (line < phoneAddressMap.length) {
+        phoneArrayMapped[keyLine] = phoneAddressMap[line][keyColumn];
+        phoneArrayMapped[typeIndex] = addressType[phone];
+        phoneArrayMapped[tagIndex] = arrayOfContents[0][phoneAddressMap[line][tagColumn]];
+        phoneArrayMapped[addressIndex] = phoneAddressMap[line][addressColumn];
+
+        keyLine = keyLine + 4;
+        typeIndex = typeIndex + 4;
+        tagIndex = tagIndex + 4;
+        addressIndex = addressIndex + 4;
+        line++;
+    }
+
+    keyLine = 0;
+    typeIndex = 1;
+    tagIndex = 2;
+    addressIndex = 3;
+    line = 0;
+
+    while (line < emailAddressMap.length) {
+        emailArrayMapped[keyLine] = emailAddressMap[line][keyColumn];
+        emailArrayMapped[typeIndex] = addressType[email];
+        emailArrayMapped[tagIndex] = arrayOfContents[0][emailAddressMap[line][tagColumn]];
+        emailArrayMapped[addressIndex] = emailAddressMap[line][addressColumn];
+
+        keyLine = keyLine + 4;
+        typeIndex = typeIndex + 4;
+        tagIndex = tagIndex + 4;
+        addressIndex = addressIndex + 4;
+        line++;
+    }
+
+    mappedAddressesArray = fixData(_.chunk(phoneArrayMapped, 4), _.chunk(emailArrayMapped, 4));
+
+    return mappedAddressesArray;
+}
+
+function fixData(phoneArrayMapped, emailArrayMapped) {
+    let tagColumn = 2;
+    let phoneArray = [];
+    let emailArray = [];
+    let phoneLine = 0;
+    let emailLine = 0;
+    let counter = 0;
+
+    while (phoneLine < phoneArrayMapped.length) {
+        for (let emailLine = 0; emailLine < emailArrayMapped.length; emailLine++) {
+            if (phoneArrayMapped[phoneLine].includes(emailArrayMapped[emailLine][tagColumn])) {
+                counter++;
+                break;
+            }
+        }
+        if (counter === 0) {
+            phoneArray[phoneLine] = phoneArrayMapped[phoneLine];
+        }
+        counter = 0;
+        phoneLine++;
+    }
+
+    phoneArray = _.compact(phoneArray);
+
+    counter = 0;
+    while (emailLine < emailArrayMapped.length) {
+        for (let phoneLine = 0; phoneLine < phoneArray.length; phoneLine++) {
+            if (emailArrayMapped[emailLine].includes(phoneArray[phoneLine][tagColumn])) {
+                counter++;
+                break;
+            }
+        }
+        if (counter === 0) {
+            emailArray[emailLine] = emailArrayMapped[emailLine];
+        }
+        counter = 0;
+        emailLine++;
+    }
+
+    emailArray = _.compact(emailArray);
+
+    return _.concat(phoneArray, emailArray);
+}
+
+function getTagsFixed(addressArrayMapped) {
+    let addressArrayMappedTags = [];
+    let tagColumn = 2;
+    let typeColumn = 1;
+
+    for (let line = 0; line < addressArrayMapped.length; line++) {
+        addressArrayMappedTags[line] = [addressArrayMapped[line][0], addressArrayMapped[line][1],
+        addressArrayMapped[line][tagColumn].split(" "), addressArrayMapped[line][3]];
+
+        if (addressArrayMappedTags[line][tagColumn][0] === addressArrayMappedTags[line][typeColumn]) {
+            addressArrayMappedTags[line][tagColumn] = _.pull(addressArrayMappedTags[line][tagColumn], addressArrayMappedTags[line][typeColumn]);
+        }
+    }
+
+    return addressArrayMappedTags;
+}
+
+function getEmailFixed(addressArrayMapped) {
+    let emailColumn = 3;
+    let emailTypeColumn = 1;
+    let email = "email";
+    let slash = "/";
+
+    for (let line = 0; line < addressArrayMapped.length; line++) {
+        if (addressArrayMapped[line][emailTypeColumn] === email && addressArrayMapped[line][emailColumn].includes(slash)) {
+            addressArrayMapped[line][emailColumn] = addressArrayMapped[line][emailColumn].split(slash);
+        }
+    }
+
+    return addressArrayMapped;
+}
+
+function getPhoneNumberFixed(addressArrayMapped) {
+    let phoneNumberColumn = 3;
+    let plusSign = "+";
+
+    for (let line = 0; line < addressArrayMapped.length; line++) {
+        if (phoneUtil.isPossibleNumberString(addressArrayMapped[line][phoneNumberColumn], 'BR')) {
+            let phoneNumber = phoneUtil.parseAndKeepRawInput(addressArrayMapped[line][phoneNumberColumn], 'BR');
+            phoneNumber = phoneUtil.format(phoneNumber, PNF.E164);
+            if (phoneNumber.startsWith(plusSign)) {
+                phoneNumber = phoneNumber.substring(1);
+            }
+            addressArrayMapped[line][phoneNumberColumn] = phoneNumber;
+        }
+    }
+
+    return addressArrayMapped;
+}
+
+function mapAddressToFullnames(mergedAddress, fullname) {
+    let fullnameColumn = 0;
+
+    for (let line = 0; line < mergedAddress.length; line++) {
+        mergedAddress[line][fullnameColumn] = mergedAddress[line][fullnameColumn] - 1;
+    }
+
+    for (let line = 0; line < mergedAddress.length; line++) {
+        if (mergedAddress[line + 1] != undefined) {
+            if (fullname[mergedAddress[line][fullnameColumn]] === fullname[mergedAddress[line + 1][fullnameColumn]]) {
+                mergedAddress[line + 1][fullnameColumn] = mergedAddress[line][fullnameColumn];
+            } else if (mergedAddress[line + 1][fullnameColumn] - 1 >= 0) {
+                mergedAddress[line + 1][fullnameColumn] = mergedAddress[line + 1][fullnameColumn] - 1;
+            }
+        }
+    }
+    return mergedAddress;
+}
+
 function getFullnames(arrayOfContents) {
     let fullNameCategoryRegex = /^\w*fullname\b/gi;
     let fullnamesArray = setValues(arrayOfContents, fullNameCategoryRegex);
@@ -212,18 +448,18 @@ function getEids(arrayOfContents) {
 
 function getClasses(arrayOfContents) {
     let classesCategoryRegex = /^\w*class\b/gi;
-    let classesArray = mapCategoryByFullname(arrayOfContents, fullnames, classesCategoryRegex);
-    let mappedClassesArray = setMappedValues(classesArray);
+    let classesArray = mapCategoryByFullname(arrayOfContents, classesCategoryRegex);
+    let mappedClassesArray = getMappedValues(classesArray);
     let classRegex = /\w*sala [0-9]*/gi;
     let mappedClassesArrayFormatted = getFormattedArray(mappedClassesArray, classRegex);
-    
+
     return mappedClassesArrayFormatted;
 }
 
 function getInvisible(arrayOfContents) {
     let invisibleCategoryRegex = /^\w*invisible\b/gi;
-    let invisibleArray = mapCategoryByFullname(arrayOfContents, fullnames, invisibleCategoryRegex);
-    let mappedInvisibleArray = setMappedValues(invisibleArray);
+    let invisibleArray = mapCategoryByFullname(arrayOfContents, invisibleCategoryRegex);
+    let mappedInvisibleArray = getMappedValues(invisibleArray);
     let validInvisible = checkIfTrueofFalse(mappedInvisibleArray);
 
     return validInvisible;
@@ -231,9 +467,107 @@ function getInvisible(arrayOfContents) {
 
 function getSeeAll(arrayOfContents) {
     let seeAllCategoryRegex = /^\w*see_all\b/gi;
-    let seeAllArray = mapCategoryByFullname(arrayOfContents, fullnames, seeAllCategoryRegex);
-    let mappedSeeAllArray = setMappedValues(seeAllArray);
+    let seeAllArray = mapCategoryByFullname(arrayOfContents, seeAllCategoryRegex);
+    let mappedSeeAllArray = getMappedValues(seeAllArray);
     let validSeeAll = checkIfTrueofFalse(mappedSeeAllArray);
 
     return validSeeAll;
+}
+
+function getAddressType() {
+    let addressType = ["phone", "email"];
+
+    return addressType;
+}
+
+function getPhoneAddress(arrayOfContents) {
+    let mappedPhoneAddressArray = mapPhoneAddress(arrayOfContents);
+
+    return mappedPhoneAddressArray;
+}
+
+function getEmailAddress(arrayOfContents) {
+    let mappedEmailAddressArray = mapEmailAddress(arrayOfContents);
+
+    return mappedEmailAddressArray;
+}
+
+function getAddresses(arrayOfContents) {
+    let phoneAddressMapped = getPhoneAddress(arrayOfContents);
+    let emailAddressMapped = getEmailAddress(arrayOfContents);
+    let addressType = getAddressType();
+    let mergedAddress = mergeAddressCategory(phoneAddressMapped, emailAddressMapped, addressType, arrayOfContents);
+    let mergedAddressFixedTags = getTagsFixed(mergedAddress);
+    let mergedAddressFixedPhoneNumbers = getPhoneNumberFixed(mergedAddressFixedTags);
+    let mergedAddressFixedEmail = getEmailFixed(mergedAddressFixedPhoneNumbers);
+    let mappedAddress = mapAddressToFullnames(mergedAddressFixedEmail, getFullnames(arrayOfContents));
+    mappedAddress.sort();
+    let type = getAddressCategory(mappedAddress, 1);
+    let tags = getAddressCategory(mappedAddress, 2);
+    let address = getAddressCategory(mappedAddress, 3);
+
+    let addressObject = [];
+    value = 1;
+
+    for (let line = 0; line < mappedAddress.length; line++) {
+        addressObject[line] = {
+            "key": mappedAddress[line][0],
+            "type": type[line][value],
+            "tags": tags[line][value],
+            "address": address[line][value],
+        };
+    }
+
+    return addressObject;
+}
+
+function getJsonObject() {
+    let fullnameIterator = fullname.values();
+    let eidIterator = eid.values();
+    let classesIterator = classes.values();
+    let invisibleIterator = invisible.values();
+    let seeAllIterator = seeAll.values();
+    let jsonIteration = 0;
+    let addressIteration = 0;
+    let json = [];
+
+    while (jsonIteration < fullname.size) {
+        let addressesClone = [];
+        while (addressIteration < addresses.length) {
+            if (addresses[addressIteration]["key"] == jsonIteration) {
+                addressesClone[addressIteration] = addresses[addressIteration];
+                delete addressesClone[addressIteration]["key"];
+            }
+            addressIteration++;
+        }
+        addressesClone = _.compact(addressesClone);
+        json[jsonIteration] = {
+            "fullname": fullnameIterator.next().value,
+            "eid": eidIterator.next().value,
+            "classes": classesIterator.next().value,
+            "addresses": addressesClone,
+            "invisible": invisibleIterator.next().value,
+            "see_all": seeAllIterator.next().value
+        };
+        jsonIteration++;
+        addressIteration = 0;
+    }
+
+    return json;
+}
+
+function getAddressCategory(mergedAddress, categoryColumn) {
+    let category = [];
+    let keyColumn = 0;
+    let element1 = 0;
+    let element2 = 1;
+
+    for (let line = 0; line < mergedAddress.length; line++) {
+        category[element1] = mergedAddress[line][keyColumn];
+        category[element2] = mergedAddress[line][categoryColumn];
+
+        element1 = element1 + 2;
+        element2 = element2 + 2;
+    }
+    return _.chunk(category, 2);
 }
